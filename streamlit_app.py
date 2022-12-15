c78da2dad5ee334b92d3189f6709bfa63dfdc57f
Sharanya P Gowda <58279614+haruhi29@users.noreply.github.com>
2022-12-15 16:27:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ def get_fruityvice_data(this_fruit_choice):
          
         
 #New section to display fruityvice api response
-streamlit.header("Fruityvice Fruit Advice!") 
+streamlit.header('Fruityvice Fruit Advice!') 
 try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?') 
     if not fruit_choice:
@@ -42,7 +42,7 @@ try:
         back_from_fuction = get_fruityvice_data(fruit_choice)
         streamlit.dataframe(back_from_function)
 except URLError as e:
-streamlit.stop()
+        streamlit.stop()
      
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
